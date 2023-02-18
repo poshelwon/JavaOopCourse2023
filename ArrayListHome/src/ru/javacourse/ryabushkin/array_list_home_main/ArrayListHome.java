45f210c92277d9fadb4017b1d14715900ca4fed0
poshelwon <poshelwon@yandex.ru>
2023-02-18 15:43:31 +0700

    correct ArrayListHome

--- ArrayListHome/src/ru/javacourse/ryabushkin/array_list_home_main/ArrayListHome.java
+++ ArrayListHome/src/ru/javacourse/ryabushkin/array_list_home_main/ArrayListHome.java
@@ -1,10 +1,10 @@
 package ru.javacourse.ryabushkin.array_list_home_main;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
-
 
 public class ArrayListHome {
     public static void removeEvenNumbers(ArrayList<Integer> list) {
@@ -17,41 +17,38 @@ public class ArrayListHome {
     }
 
     public static ArrayList<Integer> getUniqueValuesList(ArrayList<Integer> list) {
+        if (list.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
         ArrayList<Integer> resultList = new ArrayList<>();
 
-        try {
-            for (Integer item : list) {
-                if (!resultList.contains(item)) {
-                    resultList.add(item);
-                }
+        for (Integer item : list) {
+            if (!resultList.contains(item)) {
+                resultList.add(item);
             }
-        } catch (NullPointerException e) {
-            System.out.println("List is null");
         }
 
         return resultList;
     }
 
-    public static ArrayList<Integer> readFileNumbers(String path) throws IOException {
+    public static ArrayList<Integer> readStringsFileNumbers(String path) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(path));
+        String string;
+
         ArrayList<Integer> list = new ArrayList<>();
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(path));
-            String string;
-
-            while ((string = reader.readLine()) != null) {
-                list.add(toInt(string));
-            }
-
-            return list;
-        } catch (IOException e) {
-            System.out.println("File not found");
-
-            return null;
+        while ((string = reader.readLine()) != null) {
+            list.add(toInt(string));
         }
+
+        if (list.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return list;
     }
 
-    private static int toInt(Object obj) {
+    private static int toInt(Object obj) throws NumberFormatException {
         return Integer.parseInt(obj.toString());
     }
 
@@ -59,23 +56,35 @@ public class ArrayListHome {
         String filePath = "C:\\Users\\poshelwon\\IdeaProjects\\JavaOopCourse2023\\ArrayListHome\\numbers";
 
         try {
-            ArrayList<Integer> fileStringsList = readFileNumbers(filePath);
+            ArrayList<Integer> fileLinesList = readStringsFileNumbers(filePath);
 
-            System.out.println("Input list: " + fileStringsList);
+            System.out.println("Input list: " + fileLinesList);
 
-            if (fileStringsList != null) {
-                fileStringsList.replaceAll(ArrayListHome::toInt);
+            fileLinesList.replaceAll(ArrayListHome::toInt);
 
-                removeEvenNumbers(fileStringsList);
+            removeEvenNumbers(fileLinesList);
 
-                System.out.println("No-even list: " + fileStringsList);
-            }
-        } catch (IOException ignored) {}
+            System.out.println("Not even numbers list: " + fileLinesList);
+        } catch (FileNotFoundException e) {
+            System.err.println("[File not found]: " + filePath);
+        } catch (IllegalStateException e) {
+            System.err.println("[Empty file]: " + filePath);
+        } catch (NumberFormatException e) {
+            System.err.println("[File does not contain numbers]: " + filePath);
+        } catch (IOException e) {
+            System.err.println("[Input/output exception]");
+        }
 
-        ArrayList<Integer> numbersList =  new ArrayList<>(Arrays.asList(5, 2, 2, 3, 4, 3, 4, 5, 4)); /* null; */
+        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(5, 2, 2, 3, 4, 3, 4, 5, 4));
 
         System.out.println("List: " + numbersList);
 
-        System.out.println("Unique values list: " + getUniqueValuesList(numbersList));
+        System.out.print("Unique values list: ");
+
+        try {
+            System.out.println(getUniqueValuesList(numbersList));
+        } catch (IllegalStateException e) {
+            System.err.println("[Empty list]");
+        }
     }
 }
